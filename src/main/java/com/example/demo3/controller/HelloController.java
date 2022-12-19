cea01a2d082dd0cd258d725d6a9cacf98aceb142
shitongworld <1173673547@qq.com>
2022-12-19 16:26:25 +0800

    update error

--- src/main/java/com/example/demo3/controller/HelloController.java
+++ src/main/java/com/example/demo3/controller/HelloController.java
@@ -3,12 +3,14 @@ package java.com.example.demo3.controller;
 import java.sql.SQLOutput;
 
 public class HelloController {
-    System.out.print("111");
-    System.out.print("222");
-    System.out.print("333");
-    System.out.print("dev commit");
-    System.out.print("success merge dev");
-    System.out.print("this is master commit");
-    System.out.print("this is dev commit");
-    System.out.print("pull test");
+    public void testGit(){
+        System.out.print("111");
+        System.out.print("222");
+        System.out.print("333");
+        System.out.print("dev commit");
+        System.out.print("success merge dev");
+        System.out.print("this is master commit");
+        System.out.print("this is dev commit");
+        System.out.print("pull test");
+    }
 }
